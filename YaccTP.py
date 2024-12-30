9a35b51e36ad71c0c8afd028f306ed388e466ff8
eduardo <eduardo.prr1@gmail.com>
2024-12-30 21:31:06 +0000

    Adding Tests

--- YaccTP.py
+++ YaccTP.py
@@ -296,6 +296,31 @@ parser.idLabel = 0
 parser.memPointer = 0
 
 
+#___________________________________________________________________________________________________________#
+
+#### Main ####
+
+if len(sys.argv) > 1:
+    with open(sys.argv[1], 'r') as file:
+        assembly = parser.parse(file.read())
+        if assembly:
+            
+            if len(sys.argv) > 2:
+                with open(sys.argv[2], 'w') as output:
+                    output.write(assembly)
+                    print(f"{sys.argv[1]} compiled successfully!\nCheck the output in {sys.argv[2]}.")
+            else:
+                print(f"{sys.argv[1]} compiled successfully!")
+        else:
+            print("Empty!")
+else:
+    line = input(">")
+    while line!="\n":
+        print(parser.parse(line))
+        line = input(">")
+
+
+#___________________________________________________________________________________________________________#
 
 '''
 # Test
